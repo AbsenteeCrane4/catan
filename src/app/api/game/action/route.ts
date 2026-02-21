39ff69416fe1f62f23e828afad18769f38d7d221
AbsenteeCrane4 <haroonmohammed1244@gmail.com>
2026-02-21 21:12:03 +0000

    removed routes as i decided on socket.io

--- src/app/api/game/action/route.ts
+++
@@ -1,9 +0,0 @@
-import {pusherServer} from "@/lib/socket-client";
-import {NextResponse} from "next/server";
-
-export async function POST(request: Request) {
-    const {gameId, action} = await request.json();
-    await pusherServer.trigger(gameId, "game-action", action);
-    return NextResponse.json({message: "Action sent successfully"});
-}
-
